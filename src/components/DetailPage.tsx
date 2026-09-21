7cb2e2a8f5b79012cceb14fca679f11f2170f395
Joakim <joakim.wigsten@gmail.com>
2026-09-21 13:38:45 +0200

    Add categories to detail page

--- src/components/DetailPage.tsx
+++ src/components/DetailPage.tsx
@@ -2,10 +2,16 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import type { IProduct } from '../types/product';
 
+interface ICategory {
+  id: string;
+  title: string;
+}
+
 const DetailPage = () => {
   const { id } = useParams();
 
   const [product, setProduct] = useState<IProduct | null>(null);
+  const [categories, setCategories] =  useState<ICategory[]>([]);
 
   useEffect(() => {
     fetch("http://localhost:3000/products")
@@ -14,20 +20,39 @@ const DetailPage = () => {
 
         const product = data.find((product: IProduct) => product.id === id)
         setProduct(product || null);
-        
+  });
+
+  fetch("http://localhost:3000/categories")
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data);
+    
       });
+
   }, []);
+
     if (!product) {
     return <div>Laddar...</div>;
   }
+
+  const productCategories = categories.filter((category) =>
+    product.category.includes(category.id)
+  );
+
   return (
     <div>
-      <h1>{product.title}</h1>
+      
       <img src={`/card-images/${product.img}`} alt={product.title} />
+      <h1>{product.title}</h1>
+       <div>{productCategories.map((category) => (
+        <p key={category.id}>{category.title}</p>
+      ))}
+      </div>
       <p>{product.description}</p>
       <p>{product.price} kr</p>
       <p>Lager: {product.inventory}</p>
-      
+
+     
     </div>
   );
 }
