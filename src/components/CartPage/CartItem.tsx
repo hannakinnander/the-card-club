3bcd3481a11734a6faa50075e45737cd86877fb0
hannakinnander <kinnander95@live.se>
2026-09-24 11:47:53 +0200

    added cardItem-component and added it to cartComponent

--- src/components/CartPage/CartItem.tsx
+++ src/components/CartPage/CartItem.tsx
@@ -1,17 +1,24 @@
 import type { OrderItem } from "../../types/orderItem";
 
-const CartItem = (orderItem: OrderItem) => {
+interface IProps {
+  orderItem: OrderItem;
+  children: React.ReactNode;
+}
+//CHILDREN = Visar antal. Justerbart antal i varukorg och endast antal i checkout/confirmation
+const CartItem = ({ orderItem, children }: IProps) => {
   return (
     <div>
+      <img
+        src={`/card-images/${orderItem.product.img}`}
+        alt={orderItem.product.title}
+      />
       <div>
-        <img
-          src={`/card-images/${orderItem.product.img}`}
-          alt={orderItem.product.title}
-        />
-        <div>
-          <p>{orderItem.product.title}</p>
-          <p>`Pris/st: ${orderItem.product.title}`</p>
-        </div>
+        <p>{orderItem.product.title}</p>
+        <p>{`Pris/st: ${orderItem.product.price} SEK`}</p>
+      </div>
+      <div>
+        {children}
+        <p>{orderItem.quantity * orderItem.price} SEK</p>
       </div>
     </div>
   );
