28cef59d4765ec1c28807eb525098d90678fbf3c
hannakinnander <kinnander95@live.se>
2026-09-25 09:54:27 +0200

    made the total-calculator as a seperate component to use at both checkout and cart

--- src/components/CheckoutPage/CheckoutPage.tsx
+++ src/components/CheckoutPage/CheckoutPage.tsx
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
+import useCart from "../../hooks/useCart";
+import CartItem from "../common/CartItem";
+import Total from "../common/Total";
+
 const CheckoutPage = () => {
-  return <div>CheckoutPage</div>;
+  const { orderItems } = useCart();
+
+  return (
+    <div>
+      <Link to="/cart">Tillbaka</Link>
+      <h2>Kassan</h2>
+      <div>
+        {orderItems.map((orderItem) => (
+          <div key={orderItem.product.id}>
+            <CartItem
+              orderItem={orderItem}
+            >{`Antal: ${orderItem.quantity}`}</CartItem>
+          </div>
+        ))}
+        <Total />
+      </div>
+    </div>
+  );
 };
 
 export default CheckoutPage;
